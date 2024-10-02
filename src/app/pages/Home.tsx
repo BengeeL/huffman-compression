60244d3a2cd531910efed96e5619ae0cf988f722
Benjamin Lefebvre <benjamin.lefebvre.1024@gmail.com>
2024-10-01 20:16:42 -0400

    Add File Size Details

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -2,9 +2,12 @@
 
 import { HuffmanCompressor } from "../scripts/HuffmanCompressor/HuffmanCompressor";
 import { saveAs } from "file-saver";
-import React from "react";
+import React, { useState } from "react";
 
 const HomePage = () => {
+  const [originalSize, setOriginalSize] = useState<number | null>(null);
+  const [compressedSize, setCompressedSize] = useState<number | null>(null);
+
   //   ********** COMPRESSION **********
   const handleCompress = (
     text: string,
@@ -12,6 +15,7 @@ const HomePage = () => {
     originalFileExtension: string
   ) => {
     const compressed = HuffmanCompressor.compress(text, originalFileExtension);
+    setCompressedSize(compressed.length);
     saveCompressedFile(compressed, originalFileName);
   };
 
@@ -37,6 +41,7 @@ const HomePage = () => {
 
     const { text: decompressed, extension } =
       HuffmanCompressor.decompress(compressed);
+    setCompressedSize(compressed.length);
     saveDecompressedFile(decompressed, originalFileName, extension);
   };
 
@@ -58,6 +63,8 @@ const HomePage = () => {
     const file = event.target.files?.[0];
 
     if (file) {
+      setOriginalSize(file.size);
+
       const originalFileName = file.name.split(".").slice(0, -1).join(".");
       const originalFileExtension = file.name.split(".").pop() ?? "";
 
@@ -82,6 +89,15 @@ const HomePage = () => {
     }
   };
 
+  // Convert the file size to human-readable format
+  const formatBytes = (bytes: number) => {
+    if (bytes === 0) return "0 Bytes";
+    const k = 1024;
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + " " + sizes[i];
+  };
+
   //   ********** UI **********
   const renderFileUploadButton = (
     id: string,
@@ -90,22 +106,35 @@ const HomePage = () => {
     desc: string
   ) => (
     <div className='section'>
-      <h2>{label}</h2>
-      <h3>{desc}</h3>
-      <div
-        className='file-upload-button'
-        onClick={() => document.getElementById(id)?.click()}
-      >
-        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 384 512'>
-          <path d='M0 64C0 28.7 28.7 0 64 0L224 0l0 128c0 17.7 14.3 32 32 32l128 0 0 288c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 64zm384 64l-128 0L256 0 384 128z' />
-        </svg>
+      <div className='upload'>
+        <h2>{label}</h2>
+        <h3>{desc}</h3>
+        <div
+          className='file-upload-button'
+          onClick={() => document.getElementById(id)?.click()}
+        >
+          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 384 512'>
+            <path d='M0 64C0 28.7 28.7 0 64 0L224 0l0 128c0 17.7 14.3 32 32 32l128 0 0 288c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 64zm384 64l-128 0L256 0 384 128z' />
+          </svg>
+        </div>
+        <input
+          type='file'
+          id={id}
+          style={{ display: "none" }}
+          onChange={(e) => handleFileChange(e, isCompress)}
+        />
       </div>
-      <input
-        type='file'
-        id={id}
-        style={{ display: "none" }}
-        onChange={(e) => handleFileChange(e, isCompress)}
-      />
+
+      <div className='file-size'>
+        <p>
+          Original Size:{" "}
+          {originalSize !== null ? formatBytes(originalSize) : "N/A"}
+        </p>
+        <p>
+          Converted File Size:{" "}
+          {compressedSize !== null ? formatBytes(compressedSize) : "N/A"}
+        </p>
+      </div>
     </div>
   );
 
