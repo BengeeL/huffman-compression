34d70244f321ffa118aab779c5b7a7535b6b90e2
Benjamin Lefebvre <benjamin.lefebvre.1024@gmail.com>
2024-10-01 20:26:27 -0400

    Improve user feedback

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -41,7 +41,7 @@ const HomePage = () => {
 
     const { text: decompressed, extension } =
       HuffmanCompressor.decompress(compressed);
-    setCompressedSize(compressed.length);
+    setCompressedSize(decompressed.length);
     saveDecompressedFile(decompressed, originalFileName, extension);
   };
 
@@ -105,14 +105,14 @@ const HomePage = () => {
     label: string,
     desc: string
   ) => (
-    <div className='section'>
+    <div
+      className='section'
+      onClick={() => document.getElementById(id)?.click()}
+    >
       <div className='upload'>
         <h2>{label}</h2>
         <h3>{desc}</h3>
-        <div
-          className='file-upload-button'
-          onClick={() => document.getElementById(id)?.click()}
-        >
+        <div className='file-upload-button'>
           <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 384 512'>
             <path d='M0 64C0 28.7 28.7 0 64 0L224 0l0 128c0 17.7 14.3 32 32 32l128 0 0 288c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 64zm384 64l-128 0L256 0 384 128z' />
           </svg>
